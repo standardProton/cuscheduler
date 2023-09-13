0af56d7c6bd8c320af8c44dbf4598fceb665ee04
standardProton <alexm024680@gmail.com>
2023-09-13 12:52:14 -0600

    Initial working optimizer, CU API

--- pages/api/optimizer.js
+++ pages/api/optimizer.js
@@ -1,6 +1,12 @@
 import solver from "javascript-lp-solver/src/solver";
 import { isRangeIntersection } from "lib/utils";
 
+export function randomCost(i){ //basic seeded pseudorandom function
+    return Math.random();
+    //const n = Math.pow(i, 2)*(100/9.0)*Math.E;
+    //return n - Math.trunc(n);
+}
+
 export default async function handler(req, res){
     if (req.method != "POST"){
         res.status(405).json({error_msg: "Must be 'POST' method!"});
@@ -46,6 +52,8 @@ export default async function handler(req, res){
         return;
     }
 
+    const premium = false;
+
     const min_enroll_count = data.min_enroll_count == undefined ? preschedule.length : Math.min(data.min_enroll_count, preschedule.length);
     const model = {
         optimize: "cost",
@@ -57,6 +65,7 @@ export default async function handler(req, res){
         ints: {}
     }
 
+    let cost_count = 0;
     for (let i = 0; i < preschedule.length; i++){ //each class to be scheduled
         if (preschedule[i].title == undefined || preschedule[i].offerings == undefined || preschedule[i].offerings.length == 0){
             res.status(406).json({error_msg: "Malformatted preschedule object (Index " + i + ")"});
@@ -68,28 +77,29 @@ export default async function handler(req, res){
 
         for (let j = 0; j < Math.min(preschedule[i].offerings.length, 65); j++){ //each offering in class
             const offering = preschedule[i].offerings[j];
-            const model_var = {enrolled_count: 1, cost: Math.random()}
+            const model_var = {enrolled_count: 1, cost: randomCost(cost_count + 8)}
+            cost_count++;
             model_var["c" + i + "-enrolled"] = 1;
 
-            if (offering.full) model_var.cost += 10;
+            if (offering.full && premium) model_var.cost += 10;
 
             if (offering.meeting_times == undefined || offering.meeting_times.length == 0){
-                res.status(406).json({error_msg: "Class '" + title + "' offering #" + (j+1) + " does not define 'meeting_times'!"});
+                res.status(406).json({error_msg: "Class '" + title + "' offering #" + j + " does not define 'meeting_times'!"});
                 return;
             }
 
             let conflicts_avoid_times = false;
             for (let k = 0; k < offering.meeting_times.length; k++){ //for each time class meets in the week
                 const mtime = offering.meeting_times[k];
-                if (mtime.day == undefined || mtime.day < 0 || mtime.day > 4 || mtime.start_time == undefined || mtime.end_time == undefined || mtime.start_time < 0 || mtime.start_time > 128-1 || mtime.end_time <= mtime.start_time || mtime.end_time > 128){
-                    res.status(406).json({error_msg: "Malformatted meeting time in class '" + title + " offering #" + (j+1) + " meeting time #" + (k+1) + "! Check that the day, start_time, and end_time are correct."});
+                if (mtime.day == undefined || mtime.day < 0 || mtime.day > 4 || mtime.start_time == undefined || mtime.end_time == undefined || mtime.start_time < 0 || mtime.start_time > 140-1 || mtime.end_time <= mtime.start_time || mtime.end_time > 140){
+                    res.status(406).json({error_msg: "Malformatted meeting time in class '" + title + " offering " + j + " meeting time " + k + "! Check that the day, start_time, and end_time are correct."});
                     return;
                 }
 
                 if (isRangeIntersection([mtime.start_time, mtime.end_time], avoid_times[mtime.day])) conflicts_avoid_times = true;
                 
                 for (let time_itr = mtime.start_time; time_itr <= mtime.end_time + 1; time_itr++){ //every 5 min chunk in 1 class's meeting
-                    model_var["d" + mtime.day + "-t" + time_itr] = 1; //model time (0-128), also books 5 mins after class ends
+                    model_var["d" + mtime.day + "-t" + time_itr] = 1; //model time (0-140), also books 5 mins after class ends
                     model.constraints["d" + mtime.day + "-t" + time_itr] = {min: 0, max: 1};
                 }
             }
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,14 @@ import { CUtoModelTime, ModelToCUTime, getPreScheduleClass } from "lib/cu_utils.
 import { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Chip from "@mui/material/Chip";
 import styles from "styles/Main.module.css";
 import Image from "next/image";
 import { preschedule_json } from "lib/json/preschedule.js";
 import { example_schedule } from "lib/json/example_schedule.js";
+import {renderScheduleSVG} from "lib/utils.js";
+import { lookup_map } from "lib/json/lookup_map.js";
+import { name_map } from "lib/json/name_map.js";
 
 export async function getServerSideProps(context){
     return {
@@ -24,8 +28,12 @@ export default function Index({context}) {
     const [schedule_svg, setScheduleSVG] = useState(null);
     const [loading, setLoading] = useState(false);
     const [status_message, setStatusText] = useState("Brought to you by a fellow Buff!");
-    const [preschedule, setPreSchedule] = useState(preschedule_json);
-    const [schedule, setSchedule] = useState(example_schedule);
+    const [preschedule, setPreSchedule] = useState([]);
+    const [schedule, setSchedule] = useState({
+        classes: [],
+        avoid_times: [[], [], [], [], []]
+    });
+    const [class_suggestions, setClassSuggestions] = useState([]);
 
     useEffect(() => {
         if (typeof window == "undefined") return;
@@ -48,15 +56,40 @@ export default function Index({context}) {
         document.onkeydown = async (e) => {
             if (e.keyCode == 13){ //enter
                 const textinput = document.getElementById("class-search").value;
-                if (textinput.length == 0) return;
-                const c = await getPreScheduleClass(textinput, context.cors_anywhere);
-                console.log(c);
+                addPrescheduleClass(textinput);
             }
         }
 
         const search_box = document.getElementById("class-search");
         const searchBoxType = (e) => {
-            console.log("change to " + e.target.value);
+            const t = e.target.value;
+            const suggestions = [];
+            if (t.length == 0) return;
+
+            const word_split = t.split(" ");
+
+            if (word_split.length >= 2){
+                const code = (word_split[0] + " " + word_split[1]).toUpperCase().replace(":", "");
+                if (name_map[code] != undefined) suggestions.push(code);
+            }
+
+            for (let i = 0; i < word_split.length; i++){
+                const word = word_split[i].toLowerCase();
+                if (word.length > 3){
+                    const res = lookup_map[word];
+                    if (res != undefined) {
+                        for (let j = 0; j < res.length; j++) if (!suggestions.includes(res[j])) suggestions.push(res[j]);
+                    }
+                    if (word.charAt(word.length-1) != "s"){
+                        const res2 = lookup_map[word + "s"];
+                        if (res2 != undefined){
+                            for (let j = 0; j < res2.length; j++) if (!suggestions.includes(res2[j])) suggestions.push(res2[j]);
+                        }
+                    }
+                }
+            }
+
+            setClassSuggestions(suggestions);
         }
         search_box.addEventListener("input", searchBoxType);
 
@@ -67,86 +100,40 @@ export default function Index({context}) {
 
     }, [schedule, context]);
 
-    function renderScheduleSVG(width, height, schedule){
-        const marginx_right = 5, marginx_left = 9000/width, marginy = 5; //percent
-        const w = (100 - (marginx_left + marginx_right)), h = (100 - (2*marginy));
+    async function addPrescheduleClass(class_code, from_suggestion){
+        if (class_code.length == 0 || loading) return;
+        const spl = class_code.split(" ");
+        if (spl.length != 2 || spl[0].length != 4 || spl[1].length != 4) return; //class code format (ex. CSCI 1000)
 
-        const getX = (i) => {return (marginx_left + (i*w/5.0))}
-        const getY = (i) => {return ((i*h/10.75) + marginy)}
+        setClassSuggestions([]);
+        setLoading(true);
 
-        //const colors = ["#666A86", "#788AA3", "#92B6B1", "#B2C9AB", "#E8DDB5"] //slate palette
+        const preschedule_add = await getPreScheduleClass(class_code.toUpperCase(), context.cors_anywhere);
+        console.log(preschedule_add);
+        if (preschedule_add == null) return;
+        
+        for (let i = 0; i < preschedule_add.length; i++){
+            preschedule.push(preschedule_add[i]);
+        }
+        setPreSchedule(preschedule);
+        setLoading(false);
+        document.getElementById("class-search").value = "";
 
-        const color_key = {};
-        var color_count = 0;
-
-        return (
-            <svg width={width} height={1.2*height}>
-
-                {schedule != null && (<><g className={styles.avoid_times}>
-                    {schedule.avoid_times.map((hours_list, i) => (<g key={"avoid-day-" + i}>
-                        {hours_list.map((hour_set, j) => {
-                        if (hour_set.length < 2) return (<g key={"avoid-" + i + "-" + j}></g>);
-
-                        return (<g key={"avoid-" + i + "-" + j}>
-                            <rect x={getX(i) + "%"} y={getY(hour_set[0]/12.0) + "%"} height={(getY(hour_set[1]/12.0) - getY(hour_set[0]/12.0)) + "%"} width={(w/5) + "%"}></rect>
-                        </g>)})}
-                    </g>))}
-                </g>
-                </>)}
-
-                <g fill="white">
-                    {Array.from(new Array(11), (x, i) => i).map(i => (<g key={"horizontal-" + i}>
-                        <rect x={(marginx_left) + "%"} y={getY(i) + "%"} height="2" width={w + "%"}></rect>
-                        <text x={(0.2*marginx_left) + "%"} y ={(getY(i) + 1) + "%"}>{(((i+7) % 12) + 1) + ":00"}</text>
-                    </g>))}
-                    {Array.from(new Array(6), (x, i) => i).map(i => (<g key={"vertical-" + i}>
-                        <rect x={getX(i) + "%"} y={marginy + "%"} width="2" height={h + "%"}></rect>
-                    </g>))}
-                </g>
-
-                {schedule != null && (<>
-                <g>
-                    {schedule.classes.map((cl, i) => (<g key={"class-set-" + i}>
-                        {cl.meeting_times.map(meeting_time => {
-                            const x = getX(meeting_time.day) + 0.14, y = getY(meeting_time.start_time/12.0) + 0.08;
-                            //style={{fill: colors[i % colors.length]}}
-
-                            var color_num = color_key[cl.title];
-                            if (color_num == undefined) {
-                                color_num = color_count;
-                                color_key[cl.title] = color_num;
-                                color_count = (color_count+1) % 5;
-                            }
-
-                            return (<g key={"class-" + i + "-day-" + meeting_time.day}>
-                            <rect x={x + "%"} y={y + "%"} width={((w/5) - 0.14) + "%"} 
-                            height={(getY(meeting_time.end_time/12.0) - y) + "%"} 
-                            className={styles["palette-" + color_num]} rx="6" ry="6" key={"class-" + cl.title + "-day-" + meeting_time.day}></rect>
-                            
-                            <g style={{fill: "#FFF"}} fontSize={width > 900 ? "13pt" : "8pt"}>
-                                <text x={(x+0.5) + "%"} y={(y+2.4) + "%"} fontWeight="bold">{cl.title}</text>
-                                <text x={(x+0.5) + "%"} y={(y+5) + "%"}>{(width > 590 ? "Section " : "") + cl.section + (width > 400 ? " (" + cl.type + ")" : "")}</text>
-                            </g>
-                            </g>)
-                        })}
-                    </g>))}
-                </g>
-                </>)}
-            </svg>
-        )
     }
 
     async function submit(){
         if (loading) return;
-        if (schedule == null || schedule.classes.length == 0) return;
+        if (preschedule == null || preschedule.length == 0) return;
         setLoading(true);
+
+        console.log(preschedule);
 
         const res1 = await fetch("/api/optimizer", {
             method: "POST",
             body: JSON.stringify({
                 current_schedule: schedule,
                 preschedule,
-                min_enroll_count: 5,
+                min_enroll_count: preschedule.length,
             })
         });
         const res = await res1.json();
@@ -157,20 +144,45 @@ export default function Index({context}) {
             s.avoid_times = schedule.avoid_times;
             setSchedule(s);
             setStatusText("✅ Created optimal schedule");
-        } else console.error(res);
+        } else {
+            console.error(res);
+            setStatusText("❌ There was an error!");
+        }
     }
     
     return(
         <>
         <Head>
             <link rel="icon" href="/favicon.png"></link>
-            <title>Free CU Boulder Class Schedule Optimizer | Make the most of your semester</title>
+            <title>CU Boulder Class Schedule Optimizer | Make the most of your semester for Free!</title>
             <meta name="description" content="Cut down on your walking time and supercharge your sleep schedule with an optimized class schedule! Fit in all of your courses this year."></meta>
         </Head>
         <div className={styles.main_container}>
             <div className={styles.menu1}>
                 <div className={styles.menu1_settings}>
-                    <TextField fullWidth label="Enter your classes" sx={{input: {color: "#FFF", background: "#37373f"}}} id="class-search"></TextField>
+                    <TextField fullWidth label="Enter your classes" sx={{input: {color: "white", background: "#37373f"}}} id="class-search"></TextField>
+                    
+                    {class_suggestions.length > 0 && (<div className={styles.class_suggestions}>
+                        {class_suggestions.map((cs, i) => (
+                            <div className={styles.class_suggestion + " " + (i % 2 == 0 ? styles.class_suggestion_a : styles.class_suggestion_b)} onClick={() => addPrescheduleClass(cs)} key={"class-suggestion-" + i}>
+                                {cs + ": " + (name_map[cs] || "No Description")}
+                            </div>
+                        ))}
+                    </div>)}
+
+                    <div className={styles.class_list_chips}>
+                            {preschedule.map((cl, i) => (
+                                <div style={{margin: "5px"}} key={"class-chip-" + i}>
+                                    <Chip label={cl.title + " (" + cl.type + ")"} variant="filled" onDelete={() => {
+                                        
+                                        setPreSchedule(preschedule.filter(e => (e.title != cl.title) && (e.type != cl.type)));
+                                    }} style={{backgroundColor: "#e2e2e2"}}></Chip>
+                                </div>
+                            ))}
+                            {preschedule.length == 0 && (<div style={{paddingLeft: "5px"}}>
+                                <span style={{fontSize: "8pt", color: "rgba(255, 255, 255, 0.50)"}}>Search your classes to begin</span>
+                            </div>)}
+                    </div>
                     <div style={{position: "absolute", bottom: "10px", fontSize: "12pt", width: "calc(100% - 20px)"}}>
                         <center>
                             <span><b>{status_message}</b></span>
@@ -181,7 +193,7 @@ export default function Index({context}) {
                     {loading && (<div style={{marginTop: "6px", marginRight: "10px"}}>
                         <Image src="/loading.gif" width="32" height="32" alt="Loading"></Image>
                     </div>)}
-                    <Button variant="contained" onClick={submit} style={{backgroundColor: "#CFB87C"}}>OPTIMIZE</Button>
+                    <Button variant={loading ? "disabled" : "contained"} onClick={submit} style={{backgroundColor: "#CFB87C"}}>OPTIMIZE</Button>
                 </div>
             </div>
             <div className={styles.schedule_container}>
